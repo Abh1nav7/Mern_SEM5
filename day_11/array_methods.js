88ce89d1fb33f7a69237f6eefb10804a6be5389e
Ab1nav7 <abhinavdadwal112@gmail.com>
2026-07-29 16:23:24 +0530

    notes&tasks

---
+++ day_11/array_methods.js
@@ -0,0 +1,46 @@
+// let a = ["HTML","CSS","JS","React"]
+// console.log(a.length) //4
+
+// let b = ["HTML","CSS","JS","React"]
+// let res = b.toString() // to prinit arr as a string
+// console.log(res)
+// join-->  to combine array of string with specified seperator 
+// let a = ["HTML","CSS","JS","React"]
+// console.log(a.join('|'))
+
+// // concat--> to combine arrays
+// let a1 = [11, 22, 33, 44, 55];
+// let a2 = [66, 77, 88, 99, 100];
+// let a3 = [55, 66, 77, 88, 99];
+// let res = a1.concat(a2, a3);
+// console.log(res);
+
+
+// unshift --> It will add elements in begining of array 
+
+// let arr=[30,50,70]
+// arr.unshift(10,20)
+// console.log(arr)
+
+// arr.pop() // last 
+// console.log(arr)
+
+// arr.shift() // first element
+// console.log(arr)
+
+// let arr=[25,50,75,100,125]
+
+// arr.splice(1,3)// (a,b) -->a-->index, b--> number of elements
+// console.log(arr)
+
+// arr.splice(1,0,5,6,7)//(a,b..c)--> c elements will get add in arr 
+
+// console.log(arr)
+
+
+// slice -->
+let arr=[12,15,17,19,24]
+
+console.log(arr.slice(1,4))
+
+console.log(arr.reverse())
---
+++ day_12/test.js
@@ -0,0 +1,17 @@
+// let arr=["html","css","js"]
+// arr.shift()
+// console.log(arr)
+// let arr=["html","css","js"]
+// arr.unshift("react")
+// console.log(arr)
+
+// let arr=["html","css","js"]
+// arr.pop(1)
+// console.log(arr)
+
+
+let arr=["html","css","js","react"]
+console.log(arr.slice(1,3))
+
+
+
