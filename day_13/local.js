3c14162a3da15f10eb3c87f7ca0b2af39e701950
Ab1nav7 <abhinavdadwal112@gmail.com>
2026-08-03 21:10:27 +0530

    local storage

---
+++ day_13/local.js
@@ -0,0 +1,12 @@
+// Local Storage
+// It is an feature  of web browser which allow you to 
+// store data permanetly in your system
+
+// to store data in local storage 
+localStorage.setItem("users","Aman")
+// {"key" :data} //{"users":"Aman"}
+
+// to retrive data 
+localStorage.getItem("users")
+
+
